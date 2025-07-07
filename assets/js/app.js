9b85e058fc9062a413141f0e5814fa6a398e7423
Idertal <arath.riverar@gmail.com>
2025-07-07 17:24:37 -0600

    Revisión de código: corrección de errores en JS, comentarios agregados y prueba funcional

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); // * Se agrego el . para llamar a la clase.
+const $b = document.querySelector('.blog'); // *Se quito el # ya que se llama como clase no como id. 
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//!await solo puede ser usado dentro de una funcion async*/
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //!console.log(data); Aqui veo que se llama data pero no se ha definido.
+  const data = await response.json();// * Aquí se define data como el resultado de la petición a la API
+
+  // * Se agrego backticks para corregir la interpolacion. 
+ $n.textContent = `${data.name}`;
+ $b.textContent = `${data.blog}`;
+ $l.textContent = `${data.location}`;
+
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` // Faltaba definr bien el nombre de la variable con "$".
 }
 
 displayUser('stolinski').catch(handleError);
